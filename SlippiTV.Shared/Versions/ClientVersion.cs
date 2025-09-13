bb11d2a3fb0899ef5a641de8a381fc7cd4eb5e70
Anders Sundheim <andersundheim@gmail.com>
2025-09-13 16:05:32 -0700

    0.2.2

--- SlippiTV.Shared/Versions/ClientVersion.cs
+++ SlippiTV.Shared/Versions/ClientVersion.cs
@@ -4,7 +4,7 @@ namespace SlippiTV.Shared.Versions;
 
 public static class ClientVersion
 {
-    public const string SlippiClientVersion = "0.2.1";
+    public const string SlippiClientVersion = "0.2.2";
 
     public static async Task<bool> RequiresUpdateAsync(ISlippiTVService service)
     {
